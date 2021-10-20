8bf1473121cc022c9790898c65acf7a059b17126
SrJeck <86252502+SrJeck@users.noreply.github.com>
2021-10-20 13:33:19 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,21 @@
 <?php
+include('vendor/rmccue/requests/library/Requests.php');
+Requests::register_autoloader();
+
+$headers = array(
+	'Content-type' => 'application/json',
+	'Authorization' => 'Bearer YOUR-LOGIN-TOKEN'
+);
+
+
+$data = '{
+  "base64": "SGVsbG8gd29ybGQh",
+  "filename": "file.txt",
+  "properties": {
+    "webhooks": {
+      "status": "https://yoursite.com/webhook/{STATUS}/my-custom-id"
+    }
+  }
+}';
+
+$response = Requests::put('https://api.copyleaks.com/v3/education/submit/file/my-custom-id', $headers, $data);
