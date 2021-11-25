95bd158e2c9940b75b05c1afda78c3e6792d11c4
Mohammed Morad <moradahmed.mohammed.b.1142@gmail.com>
2021-11-25 13:36:31 +0800

    Reset and forgot password

--- forgot.php
+++ forgot.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (isset($_SESSION['user_id'])) {
+  $id = $_SESSION['user_id'];
+}
 ?>
 <!-- START DATE 8/28/2021 -->
 <!-- UPDATE DATE 10/05/2021 -->
@@ -29,27 +33,24 @@
     <a style="margin-top: 5px;" href="index.php">HOME</a>
     <a style="margin-top: 5px;" href="journals.php">JOURNALS</a>
     <a style="margin-top: 5px;" href="#">ANALYTICS</a>
-    <a style="float: right;" href="login.php"><img style="height: 25px;" src="images/profileIcon.png"></a>
+    <a style="float: right;" href="logOrProf.php"><img style="height: 25px;" src="images/profileIcon.png"></a>
     <a class="boomark" style="float: right;" href="#"><img style="height: 23px;" src="images/bookmark.png"></a>
   </div>
 
 
-  <!-- <div class="side">
-    <a href="editprofile.php"><i class="fa fa-pencil"> <b>Edit Profile </b> &#xf105;</i></a>
-    <a href="security.php"><i class='fas fa-user-shield' style="bold:none;"> Password</i></a>
-    <a href="add_article.php"><i class='fa fa-plus'><b> Add Article</b></i></a>
-  </div> -->
-  <div class="content">
-    <form action="login_check.php" method="post" enctype="multipart/form-data">
 
+
+  <div class="content">
+    <form action="reset.php" method="post" enctype="multipart/form-data">
       <div class="login-form">
         <div class="top">
           <div class="text-container">
-            <h1>LOG IN</h1>
+            <h1 style="font-size: 28px;">Forgot Password</h1>
           </div>
           <div class="icon-container">
             <svg fill="currentColor" viewBox="0 0 16 16">
-              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
+              <path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415z" />
             </svg>
           </div>
         </div>
@@ -65,38 +66,11 @@
               </div>
             </div>
           </div>
-          <div class="input-group">
-            <h4>Password</h4>
-            <div class="input-area">
-              <input type="password" id="password" name="password" />
-              <div class="input-icon">
-                <svg fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z" />
-                  <path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div class="input-group">
-            <input type="checkbox" class="btn-check">
-            <label>Remember me</label>
-          </div>
 
-          <button name="submit">Submit</button>
-        </div>
-        <div class="bottom">
-          <div class="options">
-            <a href="forgot.php">
-              <h4>Forgot your password?</h4>
-            </a>
-            <a href="registration.php">
-              <h4>Don't have an account?</h4>
-            </a>
-          </div>
+          <button name="submit">Forgot Password</button><br><br>
         </div>
       </div>
     </form>
-
   </div>
   <!-- ChatBot -->
   <div class="chat_icon">
