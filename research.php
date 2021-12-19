39992d04a15bc48ca95b4fd3a2b637e06e529df3
SrJeck <86252502+SrJeck@users.noreply.github.com>
2021-12-19 21:26:19 +0800

    Update research.php

--- research.php
+++ research.php
@@ -446,11 +446,9 @@ session_start();
     }
     ?>
     <?php
-    $dbh = new PDO("mysql:host=localhost;dbname=journal", "root", "");
-    $id = isset($_GET['id']) ? $_GET['id'] : "";
 
     $abstract = $dbh->prepare('select abstract from research where id=?');
-    $abstract->bindParam(1, $id);
+    $abstract->bindParam(1, $fetched2['id']);
     $abstract->execute();
     $display_abstract = $abstract->fetch();
     echo "
